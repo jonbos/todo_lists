8fe0484f081619033582473549f80a83b34db150
Jon Bos <bos.jon@gmail.com>
2019-06-20 15:20:01 -0500

    Can store a single item in list - multiple items failing

--- lists/tests.py
+++ lists/tests.py
@@ -9,5 +9,11 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
 
-        response=self.client.get('/')
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_POST_request(self):
+        response = self.client.post(
+            path='/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
