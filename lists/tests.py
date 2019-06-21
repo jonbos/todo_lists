a8155cad2171b776aa7461185848adfaf77c3903
Jon Bos <bos.jon@gmail.com>
2019-06-21 14:03:52 -0500

    Redirect after POST, display all list items

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
@@ -16,8 +18,29 @@ class HomePageTest(TestCase):
     def test_can_save_POST_request(self):
         response = self.client.post(
             path='/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_only_saves_items_when_necessary(self):
+        response = self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            path='/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        item_one=Item.objects.create(text='Pee')
+        item_two=Item.objects.create(text='Poop')
+
+        response=self.client.get('/')
+
+        self.assertIn('Pee', response.content.decode())
+        self.assertIn('Poop', response.content.decode())
 
 
 class ListItemTest(TestCase):
@@ -34,9 +57,8 @@ class ListItemTest(TestCase):
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item=saved_items[0]
-        second_saved_item=saved_items[1]
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, "The first ever list item")
         self.assertEqual(second_saved_item.text, "The second list item")
-
