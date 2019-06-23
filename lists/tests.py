2fa7c4a4e37f33f4a3f445a60b0c49ef15ff1949
Jon Bos <bos.jon@gmail.com>
2019-06-23 14:35:17 -0500

    adds a crude list model, associated db migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,5 @@
-import sys
-
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -16,16 +10,24 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ListItemTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retreiving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first ever list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "The second list item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -34,7 +36,9 @@ class ListItemTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, "The first ever list item")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "The second list item")
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -44,8 +48,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        item_one = Item.objects.create(text='Number one')
-        item_two = Item.objects.create(text='Number two')
+        list_=List.objects.create()
+        item_one = Item.objects.create(text='Number one', list=list_)
+        item_two = Item.objects.create(text='Number two', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
