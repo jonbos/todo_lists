9d63a52dd6c33ad71af47ce083fe811876323143
Jon Bos <bos.jon@gmail.com>
2019-06-24 17:39:32 -0500

    first steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,7 @@ from django.test import LiveServerTestCase
 
 import time
 
-MAX_WAIT_TIME = 2
+MAX_WAIT_TIME = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -114,3 +114,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Both are satisfied
+
+    def test_layout_and_styling(self):
+        # Edith visits the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        # She starts a new list and notices that the box is nicely centered there also
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
