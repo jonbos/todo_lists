ce1c501fd9a8d616f882532d9abab5af38cebbbf
Jon Bos <bos.jon@gmail.com>
2019-06-26 11:30:41 -0500

    adds environment variables for DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,10 +23,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%mn=iyk_-o25-4f$+g+fj%ed+!y5sfy*m5nue635c4_aerw#xk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ(['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG=True
+    SECRET_KEY='insecure-key-for-dev'
+    ALLOWED_HOSTS=[]
 
 # Application definition
 
