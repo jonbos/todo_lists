16ca71211ff42d10ac6398893046f05c95c03db0
Jon Bos <bos.jon@gmail.com>
2019-06-27 10:17:04 -0500

    split unit tests into two files - mid chapter 12

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -54,37 +54,6 @@ class NewItemTest(TestCase):
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retreiving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first ever list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "The second list item"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, "The first ever list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "The second list item")
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -110,8 +79,7 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'Other list number two')
 
     def test_passes_correct_list_to_template(self):
-        other_list=List.objects.create()
-        correct_list=List.objects.create()
-        response=self.client.get(f"/lists/{correct_list.id}/")
-        self.assertEqual(response.context['list'],correct_list)
-        
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f"/lists/{correct_list.id}/")
+        self.assertEqual(response.context['list'], correct_list)
